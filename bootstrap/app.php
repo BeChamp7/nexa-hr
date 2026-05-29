0f21bbe9eeee8fa37145401aae3be2f295226c8b
BeChamp7 <profury02@gmail.com>
2026-05-29 13:10:23 +0500

    Trust proxy headers so HTTPS is detected behind a tunnel/reverse proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Honor X-Forwarded-* headers so HTTPS is detected behind a proxy/tunnel.
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
